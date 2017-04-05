9d4e62b88970ff62c2fbbe1a024975c3abdf5fcb
康兵奎 <178974662@qq.com>
2017-04-05 18:02:40 +0800

    update

--- pages/memo/memo.js
+++ pages/memo/memo.js
@@ -2,79 +2,119 @@
 var utils = require('./utils.js');
 var tools = require('../../utils/tools.js');
 Page({
-  data: {
-    memo: [],
-    modalAnimate: {}
-  },
-  onLoad: function (options) {
-    // 页面初始化 options为页面跳转所带来的参数
-    let _ = this;
-    utils.getData((_memo) => {
-      console.log(_memo);
-      _.setData({
-        memo: _memo
-      });
-    });
-  },
-  updateMemoData: function (_memo) {
-    this.setData({
-      memo: _memo
-    });
-    //更新localStorage
-    utils.updateData(_memo);
-  },
-  setState: function (e) {
-    let _ = this, index = e.target.dataset.index;
-    let _memo = _.data.memo;
-    wx.showActionSheet({
-      itemList: ['进行中', '超时', '完成'],
-      success: function (res) {
-        let _seetIndex = res.tapIndex;
-        console.log('_index' + _seetIndex);
-        if (_seetIndex != 3 && _seetIndex != undefined) {
-          _memo[index].state = res.tapIndex + 1;
-          _.updateMemoData(_memo);
+    data: {
+        memo: [],
+        modalAnimate: {},
+        startX: 0,
+        moveX: 0,
+        endX: 0,
+        delBtnWidth: 180
+    },
+    onLoad: function(options) {
+        // 页面初始化 options为页面跳转所带来的参数
+        let _ = this;
+        utils.getData((_memo) => {
+            console.log(_memo);
+            _.setData({
+                memo: _memo
+            });
+        });
+    },
+    updateMemoData: function(_memo) {
+        this.setData({
+            memo: _memo
+        });
+        //更新localStorage
+        utils.updateData(_memo);
+    },
+    setState: function(e) {
+        let _ = this,
+            index = e.target.dataset.index;
+        let _memo = _.data.memo;
+        wx.showActionSheet({
+            itemList: ['进行中', '超时', '完成'],
+            success: function(res) {
+                let _seetIndex = res.tapIndex;
+                console.log('_index' + _seetIndex);
+                if (_seetIndex != 3 && _seetIndex != undefined) {
+                    _memo[index].state = res.tapIndex + 1;
+                    _.updateMemoData(_memo);
+                }
+            },
+            fail: function(res) {
+                // tools.errorDialog('状态修改失败，请重新尝试！');
+            }
+        });
+    },
+    openAddModal: function() {
+        this.setData({
+            modalAnimate: tools.modalOpen()
+        });
+    },
+    closeAddModal: function() {
+        this.setData({
+            modalAnimate: tools.modalClose()
+        });
+    },
+    addOneMemo: function(e) {
+        let _ = this;
+        let _title = e.detail.value.title.trim();
+        let _content = e.detail.value.content.trim();
+        if (!_title || !_content) {
+            tools.warnDialog('备忘名和内容不能为空！');
+            return false;
         }
-      },
-      fail: function (res) {
-        // tools.errorDialog('状态修改失败，请重新尝试！');
-      }
-    });
-  },
-  openAddModal: function () {
-    this.setData({
-      modalAnimate: tools.modalOpen()
-    });
-  },
-  closeAddModal: function () {
-    this.setData({
-      modalAnimate: tools.modalClose()
-    });
-  },
-  addOneMemo: function (e) {
-    let _ = this;
-    let _title = e.detail.value.title.trim();
-    let _content = e.detail.value.content.trim();
-    if (!_title || !_content) {
-      tools.warnDialog('备忘名和内容不能为空！');
-      return false;
-    }
-    _.closeAddModal();
-    utils.addOne(_title, _content, (data) => {
-      console.log(data);
-      _.updateMemoData(data);
-    });
-  },
-  onReady: function () {
-    // 页面渲染完成
-  },
-  onShow: function () {
-    // 页面显示
-  },
-  onHide: function () {
-    // 页面隐藏
-  },
-  onUnload: function () {
-    // 页面关闭
-  }
+        _.closeAddModal();
+        utils.addOne(_title, _content, (data) => {
+            console.log(data);
+            _.updateMemoData(data);
+        });
+    },
+    onReady: function() {
+        // 页面渲染完成
+    },
+    onShow: function() {
+        // 页面显示
+    },
+    onHide: function() {
+        // 页面隐藏
+    },
+    onUnload: function() {
+        // 页面关闭
+    },
+    touchS: function(e) {
+        if (e.touches.length == 1) {
+            this.setData({
+                //设置触摸起始点水平方向位置
+                startX: e.touches[0].clientX
+            });
+        }
+    },
+    touchM: function(e) {
+        console.log(e);
+        if (e.touches.length == 1) {
+            let _moveX = e.touches[0].clientX;
+            let _disX = this.data.startX - _moveX;
+            let _btnWidth = this.data.delBtnWidth;
+            let _style = '';
+            //判断是否是左划
+            if (_disX < 0 || _disX == 0) {
+                _style = 'left:0px;'
+            } else {
+                _style = 'left:-' + _disX + 'px;';
+                if (_disX >= _btnWidth) {
+                    _style = 'left:-' + _btnWidth + 'px;';
+                }
+            }
+            let _index = e.target.dataset.index;
+            let _data = this.data.memo;
+            console.log(_data);
+            console.log(_index);
+            _data[_index].style = _style;
+            this.setData({
+                memo: _data
+            });
+        }
+    },
+    touchE: function(e) {}
 })
--- pages/memo/utils.js
+++ pages/memo/utils.js
@@ -2,7 +2,7 @@
 var tools = require('../../utils/tools.js');
 var _key = 'kangbingkui_pro_memo';
 module.exports = {
-    addOne: function (title,content,callback) {
+    addOne: function(title, content, callback) {
         var _ = this;
         //获取当前key的数据，没有返回空数组
         _.getData((_memo) => {
@@ -10,30 +10,31 @@ module.exports = {
                 name: title,
                 date: tools.formatTime(new Date()),
                 content: content,
-                state: 0//状态0：正常 1：进行中  2：过时
+                state: 0, //状态0：正常 1：进行中  2：过时
+                style: ''
             });
             _.updateData(_memo);
             tools.showToast();
             callback(_memo);
         });
     },
-    deleteOne: function () { },
-    updateData: function (_memo) {
+    deleteOne: function() {},
+    updateData: function(_memo) {
         wx.setStorage({
             key: _key,
             data: _memo
         })
     },
-    getData: function (callback) {
+    getData: function(callback) {
         wx.getStorage({
             key: _key,
-            success: function (res) {
+            success: function(res) {
                 callback(res.data);
             },
-            fail: function (res) {
+            fail: function(res) {
                 callback([]);
             },
-            complete: function (res) {
+            complete: function(res) {
                 // complete
             }
         });
